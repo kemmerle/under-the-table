c59053e42a9620df83936b7421e17f045af90c91
kemmerle <kemmerle@umich.edu>
2019-05-14 14:00:48 -0400

    Pat got the Cambridge database seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, presence: true
+
+  has_many :posts 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
